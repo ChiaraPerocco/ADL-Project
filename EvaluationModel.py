018d08b4d5c1e6a5c63ed5e2bc63dc971dedef89
Perocco <chiara.perocco@hm.edu>
2024-12-09 14:12:45 +0100

    Added Evaluation for Vision Transformer and AlexNet

--- EvaluationModel.py
+++ EvaluationModel.py
@@ -54,6 +54,55 @@ print(resnet50_train_loss)
 epochs = list(range(1, resnet50_num_epoch + 1))
 print(epochs)
 (len(epochs) == len(resnet50_train_loss) == len(resnet50_val_loss) == len(resnet50_train_acc) == len(resnet50_val_acc))
+
+
+###################################################################################################
+#
+# Test model on AlexNet model
+#
+###################################################################################################
+### Import validation accuracy 
+alexNet_params = torch.load(os.path.join(current_dir, "Evaluation_folder", "alexNet_values.pth"))
+# Retrieve saved variables
+alexNet_hyper_params = alexNet_params['hyper_params']
+alexNet_num_epoch = alexNet_hyper_params['num_epochs']
+alexNet_val_loss = alexNet_params['val_losses']
+alexNet_val_acc = alexNet_params['val_accuracies']
+alexNet_train_loss = alexNet_params['train_losses']
+alexNet_train_acc = alexNet_params['train_accuracies']
+print(alexNet_train_acc)
+print(alexNet_val_acc)
+print(alexNet_val_loss)
+print(alexNet_train_loss)
+
+epochs = list(range(1, alexNet_num_epoch + 1))
+print(epochs)
+(len(epochs) == len(alexNet_train_loss) == len(alexNet_val_loss) == len(alexNet_train_acc) == len(alexNet_val_acc))
+
+
+###################################################################################################
+#
+# Test model on Vision Transformer model
+#
+###################################################################################################
+### Import validation accuracy 
+ViT_params = torch.load(os.path.join(current_dir, "Evaluation_folder", "ViT_values.pth"))
+# Retrieve saved variables
+ViT_hyper_params = ViT_params['hyper_params']
+ViT_num_epoch = ViT_hyper_params['num_epochs']
+ViT_val_loss = ViT_params['val_losses']
+ViT_val_acc = ViT_params['val_accuracies']
+ViT_train_loss = ViT_params['train_losses']
+ViT_train_acc = ViT_params['train_accuracies']
+print(ViT_train_acc)
+print(ViT_val_acc)
+print(ViT_val_loss)
+print(ViT_train_loss)
+
+epochs = list(range(1, resnet50_num_epoch + 1))
+print(epochs)
+(len(epochs) == len(ViT_train_loss) == len(ViT_val_loss) == len(ViT_train_acc) == len(ViT_val_acc))
+
 """
 # get test loader
 if False:
@@ -278,16 +327,21 @@ fig = plt.subplots(figsize =(12, 8))
 # set height of bar 
 AlexNet_bar = [f1_alexNet, test_acc_alexNet, precision_alexNet, recall_alexNet] 
 ResNet50_bar = [f1_resNet50, test_acc_resNet50, precision_resNet50, recall_resNet50] 
+ViT_bar = [f1_ViT, test_acc_ViT, precision_ViT, recall_ViT] 
 
 # Set position of bar on X axis 
 br1 = np.arange(len(AlexNet_bar)) 
-br2 = [x + barWidth for x in br1] 
+br2 = [x + barWidth for x in br1]
+br3 = [x + barWidth for x in br2]
 
 # Make the plot
 plt.bar(br1, AlexNet_bar, color ='r', width = barWidth, 
         edgecolor ='grey', label ='AlexNet') 
 plt.bar(br2, ResNet50_bar, color ='g', width = barWidth, 
-        edgecolor ='grey', label ='ResNet50') 
+        edgecolor ='grey', label ='ResNet50')
+plt.bar(br2, ResNet50_bar, color ='b', width = barWidth, 
+        edgecolor ='grey', label ='ViT')
+
 
 # Adding Xticks 
 plt.xlabel('Comparison of the classification methods in the testing stage', fontweight ='bold', fontsize = 15) 
@@ -355,6 +409,112 @@ ax2.legend(loc='lower left')
 plt.show()
 
 ###################################################################################################
+### Evaluation: AlexNet
+###################################################################################################
+
+### Confusion Matrix:
+conf_matrix_alexNet = confusion_matrix(all_labels_alexNet, all_preds_alexNet)
+
+# Visualizing Confusion Matrix
+plt.figure(figsize=(10, 7))
+sns.heatmap(conf_matrix_alexNet, annot=True, fmt='g', cmap='Blues', cbar=False)
+plt.title('Confusion Matrix AlexNet')
+plt.xlabel('Predicted labels')
+plt.ylabel('True labels')
+plt.show()
+
+# Validation and training Loss/accuracy
+epochs = list(range(1, alexNet_num_epoch + 1))
+print(epochs)
+
+plt.figure(figsize=(10, 6))
+
+plt.plot(epochs, alexNet_train_loss, label = "train loss") # plotting the Loss curve
+plt.plot(epochs, alexNet_val_loss, label = "validation loss")
+plt.plot(epochs, alexNet_train_acc, label = "train accuracy") # plotting accuracy
+plt.plot(epochs, alexNet_val_acc, label = "validation accuracy")
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+# Plot with two y-axis one for accuracy, one for loss
+fig, ax1 = plt.subplots(figsize=(10, 6))
+
+# Plot loss on the left y-axis
+ax1.plot(epochs, alexNet_train_loss, label="train loss", color='r')
+ax1.plot(epochs, alexNet_val_loss, label="validation loss", color='b')
+ax1.set_xlabel('Epoch')
+ax1.set_ylabel('Loss')
+ax1.tick_params(axis='y')
+
+# Create another y-axis for accuracy
+ax2 = ax1.twinx()
+ax2.plot(epochs, alexNet_train_acc, label="train accuracy", color='g')
+ax2.plot(epochs, alexNet_val_acc, label="validation accuracy", color='y')
+ax2.set_ylabel('Accuracy')
+ax2.tick_params(axis='y')
+
+# Add legends
+ax1.legend(loc='upper left')
+ax2.legend(loc='lower left')
+
+plt.show()
+
+###################################################################################################
+### Evaluation: Vision Transformer
+###################################################################################################
+
+### Confusion Matrix:
+conf_matrix_ViT = confusion_matrix(all_labels_ViT, all_preds_ViT)
+
+# Visualizing Confusion Matrix
+plt.figure(figsize=(10, 7))
+sns.heatmap(conf_matrix_ViT, annot=True, fmt='g', cmap='Blues', cbar=False)
+plt.title('Confusion Matrix Vision Transformer')
+plt.xlabel('Predicted labels')
+plt.ylabel('True labels')
+plt.show()
+
+# Validation and training Loss/accuracy
+epochs = list(range(1, ViT_num_epoch + 1))
+print(epochs)
+
+plt.figure(figsize=(10, 6))
+
+plt.plot(epochs, ViT_train_loss, label = "train loss") # plotting the Loss curve
+plt.plot(epochs, ViT_val_loss, label = "validation loss")
+plt.plot(epochs, ViT_train_acc, label = "train accuracy") # plotting accuracy
+plt.plot(epochs, ViT_val_acc, label = "validation accuracy")
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+# Plot with two y-axis one for accuracy, one for loss
+fig, ax1 = plt.subplots(figsize=(10, 6))
+
+# Plot loss on the left y-axis
+ax1.plot(epochs, ViT_train_loss, label="train loss", color='r')
+ax1.plot(epochs, ViT_val_loss, label="validation loss", color='b')
+ax1.set_xlabel('Epoch')
+ax1.set_ylabel('Loss')
+ax1.tick_params(axis='y')
+
+# Create another y-axis for accuracy
+ax2 = ax1.twinx()
+ax2.plot(epochs, ViT_train_acc, label="train accuracy", color='g')
+ax2.plot(epochs, ViT_val_acc, label="validation accuracy", color='y')
+ax2.set_ylabel('Accuracy')
+ax2.tick_params(axis='y')
+
+# Add legends
+ax1.legend(loc='upper left')
+ax2.legend(loc='lower left')
+
+plt.show()
+
+###################################################################################################
 ###Confusion Matrix
 ###################################################################################################
 # Confusion Matrix: ResNet50
@@ -386,7 +546,7 @@ if False:
 """
 """
     # Confusion Matrix: AlexNet
-    conf_matrix_alexNet = confusion_matrix(all_labels_resNet50, all_preds_resNet50)
+    conf_matrix_alexNet = confusion_matrix(all_labels_alexNet, all_preds_alexNet)
 
     # Visualisierung der Confusion Matrix
     plt.figure(figsize=(10, 7))
